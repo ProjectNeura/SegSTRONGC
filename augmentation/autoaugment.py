46e97703af5c255d88b9e2592b70a942f3c3a2f1
ATATC <futerry@outlook.com>
2024-08-21 16:51:13 -0400

    Added `AutoAugment`.

--- augmentation/autoaugment.py
+++ augmentation/autoaugment.py
@@ -1,3 +1,5 @@
+from random import choice as _choice
+
 from torch import tensor as _tensor
 from torchvision.transforms.functional import rotate as _rotate, adjust_brightness as _adjust_brightness, \
     adjust_saturation as _adjust_saturation, adjust_contrast as _adjust_contrast, \
@@ -5,7 +7,7 @@ from torchvision.transforms.functional import rotate as _rotate, adjust_brightne
     autocontrast as _autocontrast, equalize as _equalize, invert as _invert
 
 from augmentation.computational import npndarray as _npndarray, nparray as _nparray
-from augmentation.transform import TransformBase
+from augmentation.transform import TransformBase, Compose
 
 
 class Rotate(TransformBase):
@@ -93,3 +95,40 @@ class Invert(TransformBase):
 
     def apply(self, img: _npndarray) -> _npndarray:
         return _nparray(_invert(_tensor(img)))
+
+
+_IMAGE_NET_POLICY: tuple[Compose, ...] = (
+    Compose(Posterize(8, p=0.4), Rotate(9, p=0.6)),
+    Compose(Solarize(5, p=0.6), AutoContrast(p=0.6)),
+    Compose(Equalize(p=0.8), Equalize(p=0.6)),
+    Compose(Posterize(7, p=0.6), Posterize(6, p=0.6)),
+    Compose(Equalize(p=0.4), Solarize(4, p=0.2)),
+    Compose(Equalize(p=0.4), Rotate(8, p=0.8)),
+    Compose(Solarize(3, p=0.6), Equalize(p=0.6)),
+    Compose(Posterize(5, p=0.8), Equalize()),
+    Compose(Rotate(3, p=0.2), Solarize(8, p=0.6)),
+    Compose(Equalize(p=0.6), Posterize(6, p=0.4)),
+    Compose(Rotate(8, p=0.8), Color(0, p=0.4)),
+    Compose(Rotate(9, p=0.4), Equalize(p=0.6)),
+    Compose(Equalize(p=0), Equalize(p=0.8)),
+    Compose(Invert(p=0.6), Equalize()),
+    Compose(Color(4, p=0.6), Contrast(8)),
+    Compose(Rotate(8, p=0.8), Color(2)),
+    Compose(Color(8, p=0.8), Solarize(7, p=0.8)),
+    Compose(Sharpness(7, p=0.4), Invert(p=0.6)),
+    Compose(Sharpness(7, p=0.4), Invert(p=0.6)),
+    Compose(Color(0, p=0.4), Equalize(p=0.6)),
+    Compose(Equalize(p=0.4), Solarize(4, p=0.2)),
+    Compose(Solarize(5, p=0.6), AutoContrast(p=0.6)),
+    Compose(Invert(p=0.6), Equalize()),
+    Compose(Color(4, p=0.6), Contrast(8)),
+    Compose(Equalize(p=0.8), Equalize(p=0.6)),
+)
+
+
+class AutoAugment(TransformBase):
+    def __init__(self, p: float = 1) -> None:
+        super().__init__(p)
+
+    def apply(self, img: _npndarray) -> _npndarray:
+        return _choice(_IMAGE_NET_POLICY)(img)
