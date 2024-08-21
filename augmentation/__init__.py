93d9d9ca0fd7b077526fadb1c305c43ced45a066
ATATC <futerry@outlook.com>
2024-08-21 18:05:28 -0400

    Routine updates.

--- augmentation/__init__.py
+++ augmentation/__init__.py
@@ -1,12 +1,13 @@
 from os import listdir as _listdir
 from random import randint as _randint, choice as _choice
-from typing import Sequence as _Sequence
+from typing import Sequence as _Sequence, Callable as _Callable
 
 from cv2 import imread as _imread
 from numpy import ndarray as _ndarray, load as _load
 from torch import Tensor as _Tensor, tensor as _tensor
 
 from augmentation.autoaugment import *
+from augmentation.computational import nparray as _nparray
 from augmentation.transform import *
 
 
@@ -28,3 +29,10 @@ def load_targets(targets: _Sequence[str]) -> tuple[list[_ndarray], list[_ndarray
 def load_targets_tensor(targets: _Sequence[str]) -> tuple[list[_Tensor], list[_Tensor]]:
     return [_tensor(_imread(f"{target}.png")) for target in targets], [
         _tensor(_load(f"{target.replace('regular', 'ground_truth')}.npy")) for target in targets]
+
+
+def wrap(trans: TransformBase) -> _Callable[[_Tensor], _Tensor]:
+    def _(img: _Tensor) -> _Tensor:
+        return _tensor(trans(_nparray(img)))
+
+    return _
