52839c080cdf686df014b36b8f18c24b55fa104d
ATATC <futerry@outlook.com>
2024-08-23 14:33:14 -0400

    Implemented dataset transfer.

--- dataset_transfer.py
+++ dataset_transfer.py
@@ -1,5 +1,9 @@
-from os import makedirs
+from os import makedirs, listdir
+from shutil import copyfile
 from os.path import exists
+from rich.progress import Progress
+from numpy import load
+from cv2 import imwrite
 
 
 def check_or_create(path: str) -> None:
@@ -7,5 +11,30 @@ def check_or_create(path: str) -> None:
         makedirs(path)
 
 
-def transfer(origin: str, destination: str) -> None:
-    pass
+def transfer(src: str, destination: str) -> None:
+    destination = f"{destination}/nnUNet_raw/Dataset001_SegSTRONG_C"
+    check_or_create(destination)
+    with open(f"{destination}/dataset.json", "w") as f:
+        f.write("{\"channel_names\": {\"0\": \"red\", \"1\": \"green\", \"2\": \"blue\"}, "
+                "{\"labels\": {\"background\": 0, \"tool\": 1}, \"numTraining\": 6600, \"file_ending\": \".png\"")
+    check_or_create(f"{destination}/imagesTr")
+    check_or_create(f"{destination}/labelsTr")
+    with Progress() as progress:
+        task = progress.add_task("[white]Transferring...", total=3300)
+        serial = 0
+        for i in listdir(src):
+            for j in listdir(f"{src}/{i}"):
+                for n in range(300):
+                    copyfile(f"{src}/{i}/{j}/regular/left/{n}.png",
+                             f"{destination}/imagesTr/{(serial := str(serial).zfill(3))}_0000.png")
+                    imwrite(f"{destination}/labelsTr/{serial}.png", load(f"{src}/{i}/{j}/ground_truth/left/{n}.npy"))
+                    serial += 1
+                    copyfile(f"{src}/{i}/{j}/regular/right/{n}.png",
+                             f"{destination}/imagesTr/{(serial := str(serial).zfill(3))}_0000.png")
+                    imwrite(f"{destination}/labelsTr/{serial}.png", load(f"{src}/{i}/{j}/ground_truth/right/{n}.npy"))
+                    progress.update(task, advance=1)
+
+
+if __name__ == '__main__':
+    transfer("F:/SharedDatasets/SegSTRONGC_release/SegSTRONGC_release",
+             "F:/SharedDatasets/SegSTRONGC_release/SegSTRONGC_nnunet")
